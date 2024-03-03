166a0cfb65943db86e5b5d8f11dd01bb5c7b30b0
vanten-s <vanten-s@vanten-s.com>
2024-03-03 20:55:05 +0100

    Seperated out endpoints to a seperate file

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,6 @@
-use actix_web::{
-    get, http::header::ContentType, middleware, web, App, HttpResponse, HttpServer, Responder,
-};
-use std::{fs, io, path};
+mod endpoints;
 
-#[get("/protokoll/{ar}/{manad}/{dag}")]
-async fn protokoll(argument: web::Path<(u32, u32, u32)>) -> impl Responder {
-    let path = path::PathBuf::from(format!(
-        "assets/protokoll/{:04} {:02} {:02}.html",
-        argument.0, argument.1, argument.2
-    ));
-    match fs::read(path) {
-        Ok(value) => HttpResponse::Ok()
-            .content_type(ContentType::html())
-            .body(value),
-        Err(err) => match err.kind() {
-            io::ErrorKind::NotFound => HttpResponse::NotFound()
-                .content_type(ContentType::plaintext())
-                .body("Finns inget protokoll från den dagen :/"),
-            _ => HttpResponse::InternalServerError()
-                .content_type(ContentType::plaintext())
-                .body("Servern funkar inte :/"),
-        },
-    }
-}
+use actix_web::{middleware, App, HttpServer};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
@@ -34,7 +12,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .wrap(middleware::Compress::default())
             .wrap(middleware::Logger::default())
-            .service(protokoll)
+            .service(endpoints::protokoll)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
