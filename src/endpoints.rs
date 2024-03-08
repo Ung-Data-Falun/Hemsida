6e7a41195753c304ac350c0cc7934cfac4e7bcb9
vanten-s <vanten-s@vanten-s.com>
2024-03-08 02:06:37 +0100

    Fixade protokoll listor (klockan är 02:06)

--- src/endpoints.rs
+++ src/endpoints.rs
@@ -1,30 +1,62 @@
-use actix_web::{get, http::header::ContentType, web::Path, HttpResponse, Responder};
-use std::{fs, io, path::PathBuf};
+use actix_web::{get, web::Path, Responder};
+
+#[get("/api/protokoll/{år}/{månad}/")]
+pub async fn api_protokoll_i_månad(argument: Path<(String, String)>) -> impl Responder {
+    let path = format!("assets/static/protokoll_raw/{}/{}", argument.0, argument.1);
+    ung_data_backend::ul_filer(&path, "Inga protokoll denna tidsperiod :/", |filename: &str, _path: &str| -> String {
+        let datum = filename.split(".").next().unwrap_or("");
+        let link = datum.replace('-', "/");
+        let link = format!("/protokoll/{link}");
+        format!("<li><a href=\"{link}\">{datum}</a></li>")
+    })
+}
+
+#[get("/api/protokoll/{år}/")]
+pub async fn api_protokoll_i_år(argument: Path<String>) -> impl Responder {
+    let path = format!("assets/static/protokoll_raw/{argument}");
+    ung_data_backend::ul_filer(&path, "Inga protokoll denna tidsperiod :/", move |filename: &str, _path: &str| -> String {
+        let link = format!("{filename}/");
+        format!("<li><a href=\"{link}\">{filename}</a></li>")
+    })
+}
+
+#[get("/api/protokoll/")]
+pub async fn api_protokoll() -> impl Responder {
+    let path = format!("assets/static/protokoll_raw");
+    ung_data_backend::ul_filer(&path, "Inga protokoll :/", move |filename: &str, _path: &str| -> String {
+        let link = format!("{filename}/");
+        format!("<li><a href=\"{link}\">{filename}</a></li>")
+    })
+}
+
+#[get("/protokoll/{år}/{månad}/")]
+pub async fn protokoll_i_månad(argument: Path<(String, String)>) -> impl Responder {
+    ung_data_backend::mall(
+        &format!("Protokoll {} {}", argument.0, argument.1),
+        &format!(
+            "<div class=\"replace\" href=\"/api/protokoll/{}/{}/\"></div>",
+            argument.0, argument.1
+        ),
+    )
+}
+
+#[get("/protokoll/{år}/")]
+pub async fn protokoll_i_år(argument: Path<String>) -> impl Responder {
+    ung_data_backend::mall(
+        &format!("Protokoll {argument}"),
+        &format!(
+            "<div class=\"replace\" href=\"/api/protokoll/{argument}/\"></div>",
+        ),
+    )
+}
 
 #[get("/protokoll/{år}/{månad}/{dag}")]
-pub async fn protokoll(argument: Path<(u32, u32, u32)>) -> impl Responder {
-    let path = PathBuf::from(format!(
-        "assets/protokoll/{:04}/{:02}/{:04} {:02} {:02}.html",
-        argument.0, argument.1, argument.0, argument.1, argument.2
-    ));
-    match fs::read(path) {
-        Ok(value) => HttpResponse::Ok()
-            .content_type(ContentType::html())
-            .body(value),
-        Err(err) => match err.kind() {
-            io::ErrorKind::NotFound => HttpResponse::NotFound()
-                .content_type(ContentType::plaintext())
-                .body("Finns inget protokoll från den dagen :/"),
-            _ => HttpResponse::InternalServerError()
-                .content_type(ContentType::plaintext())
-                .body("Servern funkar inte :/"),
-        },
-    }
+pub async fn protokoll(argument: Path<(String, String, String)>) -> impl Responder {
+    ung_data_backend::mall(
+        &format!("Protokoll {} {} {}", argument.0, argument.1, argument.2),
+        &format!(
+            "<div class=\"replace\" href=\"/protokoll_raw/{}/{}/{}-{}-{}.html\"></div>",
+            argument.0, argument.1, argument.0, argument.1, argument.2
+        ),
+    )
 }
-
-/*
-#[get("/protokoll/{år}/{månad}")]
-pub async fn protokoll_i_månad(argument: Path<(u32, u32)>) -> impl Responder {
-
-}
-*/
