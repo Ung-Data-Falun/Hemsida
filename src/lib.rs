2c60bbfd34241473651dd0c4440ad584d06396fc
vanten-s <vanten-s@vanten-s.com>
2024-03-10 17:33:43 +0100

    Tog bort onödig kod

--- src/lib.rs
+++ src/lib.rs
@@ -1,52 +1,5 @@
 use actix_web::{http::header::ContentType, HttpResponse, Responder};
-use std::{fs, io, path::PathBuf};
-
-pub async fn protokoll_i_månad(år: u32, månad: u32) -> io::Result<Vec<(String, String)>> {
-    let path = PathBuf::from(format!("assets/protokoll/{:04}/{:02}/", år, månad));
-
-    let read_dir = fs::read_dir(path)?;
-
-    let mut filnamn = Vec::new();
-
-    for dir_entry in read_dir {
-        let dir_entry = dir_entry?;
-        let nuvarande_filnamn = dir_entry.file_name();
-        let nuvarande_filnamn = match nuvarande_filnamn.to_str() {
-            Some(value) => value,
-            None => {
-                log::error!("Kunde inte få en &str från OsString");
-                continue;
-            }
-        };
-        let utan_filtyp = match nuvarande_filnamn.split('.').next() {
-            Some(value) => value,
-            None => {
-                log::warn!("Ingen filtyp på {nuvarande_filnamn}");
-                continue;
-            }
-        };
-        let mut delar_i_filnamn = utan_filtyp.split(' ');
-        let år = match delar_i_filnamn.next() {
-            Some(value) => value,
-            None => continue,
-        };
-        let månad = match delar_i_filnamn.next() {
-            Some(value) => value,
-            None => continue,
-        };
-        let dag = match delar_i_filnamn.next() {
-            Some(value) => value,
-            None => continue,
-        };
-
-        filnamn.push((
-            format!("{år} {månad} {dag}"),
-            format!("{år}/{månad}/{år} {månad} {dag}"),
-        ))
-    }
-
-    Ok(filnamn)
-}
+use std::fs;
 
 pub fn ul_filer(
     path: &str,
